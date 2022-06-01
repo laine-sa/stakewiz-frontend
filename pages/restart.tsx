58ddae13458035322b2d3f9f027453a904dfa29a
Michael Hubbard <michaelh@laine.co.za>
2022-06-01 22:49:02 +0200

    restartcomplete

--- pages/restart.tsx
+++ pages/restart.tsx
@@ -25,28 +25,6 @@ const getStake = async () => {
 
 export default function Home() {
 
-    const [stake, setStake] = useState(null);
-
-
-    const getStake = () => {
-        axios(API_URL+config.API_ENDPOINTS.gossip_stake, {
-            headers: {'Content-Type':'application/json'}
-        })
-          .then(response => {
-            let json = response.data;
-            setStake(json.active_stake);
-          })
-          .catch(e => {
-            console.log(e);
-          })
-    }
-
-    setTimeout(() => {
-            getStake()
-        },
-        5000
-    );
-
 
     return (
         <div>
@@ -57,21 +35,11 @@ export default function Home() {
             <main>
                 <TopBar />
 
-                {(stake!=null) ? (
+                
                     <div className='container'>         
                         <div className='text-center'>
-                            <h2 className='text-white'>Cluster restart is underway</h2>
-                        </div>       
-                        <div className="d-flex justify-content-center my-5">                    
-                            <div className="w-50 progress" data-bs-toggle="tooltip" title="See FAQ for formula of this display." data-bs-placement="bottom" style={{height:'30px'}}>                        
-                                <div className={"progress-bar progress-bar-striped progress-bar-animated bg-success"} role="progressbar"  style={{width: stake+'%'}}>
-                                    {stake+'%'}
-                                </div>                    
-                            </div>                
-                        </div>    
-                        <div className='text-center text-white fs-5'>
-                            Bar above shows active stake visible in gossip. Once 80% is active the network will restart.
-                        </div>          
+                            <h2 className='text-white'>Network has restarted.</h2>
+                        </div>             
                         <div className='text-center text-white fs-6 my-3'>
                             <p>This display was rapidly built to provide insight into the restart. It receives data directly from our validator log every 5 seconds, refresh to update.</p>
                             <p>Built by <a href='http://laine.co.za/solana' target='_new'>Laine</a> - a Solana Validator</p>
@@ -79,13 +47,7 @@ export default function Home() {
                             <p>Information is provided without warranty for it&apos;s accuracy.</p>
                         </div>         
                     </div>     
-                ) : (
-                    <div className='container'>         
-                        <div className='text-center'>
-                            <h2 className='text-white'>Loading...</h2>
-                        </div></div>
-                    )}
-                
+               
             </main>
 
             <Footer />
