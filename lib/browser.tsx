c06310778add61c19feecbc5677b7740631f3aa5
Bedh Prakash Roy <bedh@laine.co.za>
2022-09-12 19:16:40 +0530

    disable the console log

--- lib/browser.tsx
+++ lib/browser.tsx
@@ -1,7 +1,7 @@
 // Safari browser check
 export const check = (name:string) => {
     if (typeof window !== "undefined") {
-        console.log(`check for : ${name} , user's device is: ${window.navigator.userAgent}`);
+        // console.log(`check for : ${name} , user's device is: ${window.navigator.userAgent}`);
         return navigator.userAgent.indexOf(name) != -1;
     }else{
         return false;
