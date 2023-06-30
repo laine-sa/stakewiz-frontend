568b7a39fc0a98c50adb892bbb10ab5622375287
Michael <68944931+michaelh-laine@users.noreply.github.com>
2023-06-30 11:00:04 +0200

    Add media (#85)
    * Update email logo
    
    * Add media.stakewiz.com

--- next.config.js
+++ next.config.js
@@ -10,6 +10,6 @@ module.exports = {
     API_BASE_URL: process.env.API_BASE_URL
   },
   images: {
-    domains: ['s3.amazonaws.com']
+    domains: ['s3.amazonaws.com', 'media.stakewiz.com']
   }
 }
