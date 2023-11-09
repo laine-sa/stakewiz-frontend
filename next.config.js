0dade381938869c381115d2eb6fb00e33fd341cc
Michael Hubbard <michaelh@laine.co.za>
2023-11-09 22:11:41 +0200

    Add maintenance mode

--- next.config.js
+++ next.config.js
@@ -11,5 +11,12 @@ module.exports = {
   },
   images: {
     domains: ['s3.amazonaws.com', 'media.stakewiz.com']
+  },
+  redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === "1"
+        ? { source: "/((?!maintenance).*)", destination: "/maintenance", permanent: false }
+        : null,
+    ].filter(Boolean);
   }
 }
